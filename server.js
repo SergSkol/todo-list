7069d074340f849bad5502237ef6b01e67492970
Serg Skol <sps676@gmail.com>
2022-11-08 15:04:18 +0100

    Set an array of some simple to do tasks

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-/* eslint-disable */
 const express = require('express');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 
 const app = express();
 const config = require('./webpack.config.js');
+
 const compiler = webpack(config);
 
 // Tell express to use the webpack-dev-middleware and use the webpack.config.js
@@ -12,10 +12,10 @@ const compiler = webpack(config);
 app.use(
   webpackDevMiddleware(compiler, {
     publicPath: config.output.publicPath,
-  })
+  }),
 );
 
 // Serve the files on port 3000.
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log('Example app listening on port 3000!\n');
 });
--- src/index.js
+++ src/index.js
@@ -1,19 +1,35 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 import './style.css';
 import ToDoList from './modules/ToDoList.js';
-
-console.log(_);
 
 const toDoAdd = document.querySelector('#add');
 
 const myToDoList = new ToDoList();
 
 toDoAdd.addEventListener('change', () => {
-  myToDoList.addTask();
-  myToDoList.saveTasks();
+  // myToDoList.addTask();
+  // myToDoList.saveTasks();
 });
 
 window.onload = () => {
-  myToDoList.loadTasks();
+  // myToDoList.loadTasks();
   myToDoList.showTasks();
 };
+
+myToDoList.arr = [
+  {
+    index: 1,
+    description: 'Morning session',
+    completed: false,
+  },
+  {
+    index: 2,
+    description: 'Coding with partners',
+    completed: false,
+  },
+  {
+    index: 3,
+    description: 'Stand up session',
+    completed: false,
+  },
+];
