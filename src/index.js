ff7339179c6f4b9ed3cf2f17261dc9c44d992b22
Serg Skol <sps676@gmail.com>
2022-11-08 19:23:54 +0100

    Add interactive checkbox

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 // import _ from 'lodash';
-// import './style.css';
+import './style.css';
 import ToDoList from './modules/ToDoList.js';
 
 const toDoAdd = document.querySelector('#add');
+const toDoClear = document.querySelector('.todo-clear');
 
 const myToDoList = new ToDoList();
 
 toDoAdd.addEventListener('change', () => {
   myToDoList.addTask();
+  myToDoList.saveTasks();
+});
+
+toDoClear.addEventListener('click', () => {
+  myToDoList.clearCompletedTasks();
   myToDoList.saveTasks();
 });
 
